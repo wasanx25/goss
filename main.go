8da7f5945c8e489591b2b0c7907948d537f48c96
wataru0225 <wataru.kikuchi0225@gmail.com>
2018-04-01 21:57:23 +0900

    Remove cursor movement

--- main.go
+++ main.go
@@ -40,16 +40,11 @@ func main() {
 
 loop:
 	for {
-		var y int
 		k := stdscr.GetChar()
 		switch byte(k) {
 		case 'j':
-			y, _ = stdscr.CursorYX()
-			stdscr.Move(y+1, 0)
 			stdscr.Scroll(1)
 		case 'k':
-			y, _ = stdscr.CursorYX()
-			stdscr.Move(y-1, 0)
 			stdscr.Scroll(-1)
 		case 'q':
 			stdscr.Print("oh my god.")
