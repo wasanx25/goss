2ccdb90523f9916eaf6fbae1c6948c41b382af4d
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-06-06 23:51:06 +0900

    Add view of line

--- main.go
+++ main.go
@@ -16,6 +16,16 @@ func main() {
 		fmt.Fprintf(os.Stderr, "tcell.NewScreen() error: %s", err)
 	}
 
+	data := `
+# 見出し1
+## 見出し2
+### 見出し3
+#### 見出し4
+
+
+##### 見出し5
+`
+
 	err = tui.Init()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "tui.Init() error: %s", err)
@@ -23,8 +33,13 @@ func main() {
 
 	tui.SetStyle(tcell.StyleDefault.Foreground(tcell.ColorBlueViolet).Background(tcell.ColorBlack))
 	x := 1
-	for _, s := range "ジャ-ジャ-麺" {
-		tui.SetContent(x, 1, s, nil, tcell.StyleDefault)
+	y := 1
+	for _, s := range data {
+		tui.SetContent(x, y, s, nil, tcell.StyleDefault)
+		if string(s) == "\n" {
+			x = 1
+			y++
+		}
 		x += runewidth.RuneWidth(s)
 	}
 
