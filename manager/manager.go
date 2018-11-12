9b33e5ccec6264d6b499855d6855f5fc04f23d01
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-11-13 01:12:56 +0900

    Add manager pkg

---
+++ manager/manager.go
@@ -0,0 +1,40 @@
+package manager
+
+import (
+	"fmt"
+
+	"github.com/gdamore/tcell"
+	"github.com/wasanx25/goss/window"
+)
+
+type Manager struct {
+	Window *window.Window
+	Tui    tcell.Screen
+}
+
+func NewManager() (*Manager, error) {
+	tui, err := tcell.NewScreen()
+	if err != nil {
+		err = fmt.Errorf("tcell.NewScreen() error: %s", err)
+		return nil, err
+	}
+
+	err = tui.Init()
+	if err != nil {
+		err = fmt.Errorf("tcell.tui.Init() error: %s", err)
+		return nil, err
+	}
+
+	window, err := window.New()
+	if err != nil {
+		err = fmt.Errorf("window.New() error: %s", err)
+		return nil, err
+	}
+
+	manager := &Manager{
+		Window: window,
+		Tui:    tui,
+	}
+
+	return manager, nil
+}
