d2e655533897a278835450d70db175a29e6f57d0
wasanx25 <wasanx25@gmail.com>
2018-11-23 22:51:44 +0900

    Move dir

---
+++ _samples/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/wasanx25/goss/run"
+)
+
+func main() {
+	data := `
+# 見出し1
+aa aa aa
+## 見出し2
+bb	bb	bb
+### 見出し3
+#### 見出し4
+
+
+##### 見出し5
+# 見出し1
+aa aa aa
+## 見出し2
+bb	bb	bb
+### 見出し3
+#### 見出し4
+
+
+##### 見出し5
+# 見出し1
+aa aa aa
+## 見出し2
+bb	bb	bb
+### 見出し3
+#### 見出し4
+
+
+##### 見出し5
+# 見出し1
+aa aa aa
+## 見出し2
+bb	bb	bb
+### 見出し3
+#### 見出し4
+
+
+##### 見出し5
+`
+	err := run.Exec(data)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	os.Exit(0)
+}
